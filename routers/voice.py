47b550f66a0b6e3f4e741f251e8c1b00592008c3
박진영 <tear4321@cau.ac.kr>
2024-05-02 18:34:36 +0900

    voice/ WhisperModel cuda error

--- routers/voice.py
+++ routers/voice.py
@@ -2,22 +2,21 @@ from fastapi import APIRouter
 import features.voice.clone_voice as Clone
 import features.voice.text_to_speech as TTS
 
-router = APIRouter()
-path = "/voice"
+router = APIRouter(prefix="/voice")
 
 
-@router.get(path, tags=["voice"])
+@router.get('/', tags=["voice"])
 async def voice():
     return {"message": "Voice API"}
 
 
-@router.put(path + '/clone/{user_id}', tags=["voice"])
+@router.put('/{user_id}', tags=["voice"])
 async def clone_voice(user_id: str, audio_path: str):
     Clone.clone_voice(user_id, audio_path)
     return {"result": "success"}
 
 
-@router.get(path + '/tts/{user_id}', tags=["voice"])
+@router.get('/{user_id}/speech', tags=["voice"])
 async def text_to_speech(user_id: str, text: str):
     TTS.text_to_speech(user_id, text, language='KR')
-    return {"message": "Voice API"}
+    return {"message": "text to speech success"}
