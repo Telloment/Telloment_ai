25715ec04223e1f29bbe3521410cd0956f7af657
박진영 <tear4321@cau.ac.kr>
2024-05-02 16:22:43 +0900

    voice/ uuper-case error

--- features/voice/env_vars.py
+++ features/voice/env_vars.py
@@ -5,6 +5,6 @@ from openvoice.api import ToneColorConverter
 ckpt_converter = 'OpenVoice/checkpoints_v2/converter'
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 output_dir = 'outputs_v2'
-source_se = torch.load(f'OpenVoice/checkpoints_v2/base_speakers/ses/KR.pth', map_location=device)
+source_se = torch.load(f'OpenVoice/checkpoints_v2/base_speakers/ses/kr.pth', map_location=device)
 tone_color_converter = ToneColorConverter(f'{ckpt_converter}/config.json', device=device)
 tone_color_converter.load_ckpt(f'{ckpt_converter}/checkpoint.pth')
